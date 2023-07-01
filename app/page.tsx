6b73ecb4a3a7321e271071c5c0f0698e8b45fac8
Tamer Tawfik <tamertawfiklxr@gmail.com>
2023-07-01 03:00:49 +0000

    Added: My Personal Info

--- app/page.tsx
+++ app/page.tsx
@@ -29,32 +29,30 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				Tamer Tawfik
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hi, my name is Tamer, I'm  a self-taught front end developer  and freelance at{" "}
 					<Link
 						target="_blank"
-						href="https://upstash.com"
+						href="https://upwork.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Upstash
+						Upwork
 					</Link>
 
 					<br />
-					and working on{" "}
+					and template author at{" "}
 					<Link
 						target="_blank"
-						href="https://planetfall.io"
+						href="https://www.templatemonster.com/"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						planetfall.io
-					</Link>{" "}
-					at night.
+						Template Monster
+					</Link>.
 				</h2>
 			</div>
 		</div>
